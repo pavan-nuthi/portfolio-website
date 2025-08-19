198e6fd9fd147d3ec7913538e7c9a50c178e3906
Pavan Kumar Nuthi <kupavan@athenahealth.com>
2025-08-19 23:19:26 +0530

    add media queries for diff displays

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -136,10 +136,9 @@ export const Contact = () => {
               }
             }}
           >
-            I'm actively seeking internship opportunities for Fall 2026 and Spring 2027, 
-            as well as full-time positions starting May 2027. Whether you have exciting job
+            I'm actively seeking internship opportunities for Fall 2026 and Spring 2027. Whether you have exciting job
             opportunities to discuss or just want to connect, I'd love to hear from you!
-            Please feel free to drop an email!
+            Let's build something amazing together!
           </motion.p>
         </motion.div>
         
